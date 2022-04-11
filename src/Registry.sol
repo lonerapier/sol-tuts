99455d54833060634d8a716bef316ed8cd4c0527
Sambhav Dusad <sambhavdusad24@gmail.com>
2022-04-12 02:06:29 +0530

    ✅ registry contract and tests

---
+++ src/Registry.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.12;
+
+
+/// @notice Simple registry that registers a string name to an address.
+/// @dev time period of 1 day is used to prevent spamming.
+contract Registry {
+	// ============== Custom Errors ==============
+
+	error AlreadyClaimed();
+	error EarlyClaim();
+	error Unauthorised();
+
+	// ============== Events ==============
+
+	event NameClaimed(address indexed _claimer, string _name);
+	event NameReleased(address indexed _releaser, string _name);
+
+	// ============== Public State Variables ==============
+
+	uint256 constant public CLAIM_PERIOD = 1 days;
+	mapping(string => address) public names;
+	mapping(address => uint256) public lastClaimedAt;
+
+	// ============== Public FUnctions ==============
+
+	function claim(string memory name) public payable {
+		if (names[name] != address(0)) revert AlreadyClaimed();
+		if (block.timestamp < lastClaimedAt[msg.sender] + CLAIM_PERIOD) 	revert EarlyClaim();
+
+		lastClaimedAt[msg.sender] = block.timestamp;
+		names[name] = msg.sender;
+		emit NameClaimed(msg.sender, name);
+	}
+
+	function release(string memory name) public payable {
+		if (names[name] != msg.sender) revert Unauthorised();
+
+		names[name] = address(0);
+		emit NameReleased(msg.sender, name);
+	}
+}
---
+++ src/test/Registry.t.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.12;
+
+import {Registry} from "../Registry.sol";
+import {DSTestPlus} from "./utils/DSTestPlus.sol";
+
+contract TestRegistry is DSTestPlus {
+
+	address constant public sampleAdd = address(0xabcd);
+	string constant public sampleName = "gm";
+
+	Registry public registry;
+
+	function setUp() public {
+		registry = new Registry();
+	}
+
+	function testClaim() public {
+		registry.claim(sampleName);
+
+		assertEq(registry.names(sampleName), address(this));
+		assertEq(registry.lastClaimedAt(address(this)), block.timestamp);
+	}
+
+	function testClaimAlreadyClaimed() public {
+		registry.claim(sampleName);
+
+		vm.expectRevert(abi.encodeWithSignature("AlreadyClaimed()"));
+		registry.claim(sampleName);
+	}
+
+	function testClaimEarlyClaim() public {
+
+		registry.claim(sampleName);
+
+		vm.expectRevert(abi.encodeWithSignature("EarlyClaim()"));
+		registry.claim("gmm");
+	}
+
+	function testClaimMultipleNameSameAddress() public {
+		registry.claim(sampleName);
+
+		assertEq(registry.names(sampleName), address(this));
+		assertEq(registry.lastClaimedAt(address(this)), block.timestamp);
+
+		vm.warp(block.timestamp + 1 days);
+
+		registry.claim("gmm");
+
+		assertEq(registry.names("gmm"), address(this));
+		assertEq(registry.lastClaimedAt(address(this)), block.timestamp);
+	}
+
+	function testClaimSameNameMultipleAddress() public {
+		registry.claim(sampleName);
+
+		vm.prank(sampleAdd);
+		vm.expectRevert(abi.encodeWithSignature("AlreadyClaimed()"));
+		registry.claim(sampleName);
+	}
+
+	function testClaimMultipleNamesMultipleAddress() public {
+		registry.claim(sampleName);
+
+		vm.prank(sampleAdd);
+		registry.claim("gmm");
+
+		assertEq(registry.names("gmm"), sampleAdd);
+	}
+
+	function testClaimFuzz(string memory _name) public {
+		registry.claim(_name);
+	}
+
+	function testRelease() public {
+		registry.claim(sampleName);
+		registry.release(sampleName);
+
+		assertEq(registry.names(sampleName), address(0));
+	}
+
+	function testReleaseUnauthorised() public {
+		registry.claim(sampleName);
+
+		vm.expectRevert(abi.encodeWithSignature("Unauthorised()"));
+		vm.prank(sampleAdd);
+		registry.release(sampleName);
+	}
+
+	function testReleaseNotClaimed() public {
+		vm.expectRevert(abi.encodeWithSignature("Unauthorised()"));
+		registry.release(sampleName);
+	}
+}
