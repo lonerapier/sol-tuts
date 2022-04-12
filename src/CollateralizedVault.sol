062f6f858f4bfdaada32a5dfd947fe2eb57b9a8f
Sambhav Dusad <sambhavdusad24@gmail.com>
2022-04-13 04:22:15 +0530

    ✅ add collateralized vault contracts and tests

---
+++ src/CollateralizedVault.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.12;
+
+import {Dai} from "./Dai.sol";
+import {AggregatorV3Interface} from "./interfaces/AggregatorV3Interface.sol";
+import {DSTestPlus} from "./test/utils/DSTestPlus.sol";
+
+contract CollateralizedVault is DSTestPlus {
+
+    // ============== Custom Errors ==============
+
+    error InsufficientBalance();
+    error InsufficientCollateral();
+    error InvalidAmount();
+    error InvalidExchangeRate();
+    error Unauthorised();
+    error Safe();
+
+    // ============== Events ==============
+
+    event Deposit(address indexed depositor, uint256 amount);
+    event Withdraw(address indexed withdrawer, uint256 amount);
+    event Borrow(address indexed borrower, uint256 amount);
+    event Repay(address indexed borrower, uint256 amount);
+    event Liquidate(address indexed borrower);
+
+    // ============== Public State Variables ==============
+
+    address public immutable owner;
+    Dai public immutable token;
+    AggregatorV3Interface public immutable oracle;
+
+    mapping(address => uint256) public deposits;        // deposits is in ETH amount
+    mapping(address => uint256) public loans;           // loans is in DAI amount
+
+    // ============== Constructor ==============
+
+    constructor(address _token, address _oracle) {
+        owner = msg.sender;
+
+        token = Dai(_token);
+
+        oracle = AggregatorV3Interface(_oracle);
+    }
+
+    // ============== Public Functions ==============
+
+    function applyExchangeRate(uint256 amount) public view returns (uint256) {
+        (, int256 exchangeRate, , , ) = oracle.latestRoundData();
+        if (exchangeRate <= 0) revert InvalidExchangeRate();
+
+        return (amount * uint256(exchangeRate)) / 1e18;
+    }
+
+    /// @notice deposits ETH into the vault
+    function deposit() external payable {
+        deposits[msg.sender] += msg.value;
+
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    /// @notice borrow DAI against ETH deposits
+    /// @param _daiAmount amount of DAI to borrow
+    function borrow(uint256 _daiAmount) external {
+        uint256 depositDaiAmount = applyExchangeRate(deposits[msg.sender]);
+
+        uint256 loanAmount = loans[msg.sender];
+
+        if (_daiAmount > (depositDaiAmount - loanAmount)) revert InsufficientCollateral();
+
+        loans[msg.sender] += _daiAmount;
+
+        token.transfer(msg.sender, _daiAmount);
+
+        emit Borrow(msg.sender, _daiAmount);
+    }
+
+    /// @notice withdraw deposited collateral
+    /// @dev need to repay loan first, if withdraw amount is more than balance
+    /// i.e. deposits - loans
+    /// @param _ethAmount amount of ETH to withdraw
+    function withdraw(uint256 _ethAmount) external {
+        if (_ethAmount > deposits[msg.sender]) revert InsufficientBalance();
+
+        uint256 depositDaiAmount = applyExchangeRate(deposits[msg.sender]);
+        uint256 withdrawDaiAmount = applyExchangeRate(_ethAmount);
+
+        if (depositDaiAmount - loans[msg.sender] < withdrawDaiAmount) revert InsufficientBalance();
+        unchecked {
+            deposits[msg.sender] -= _ethAmount;
+        }
+
+        payable(msg.sender).transfer(_ethAmount);
+
+    }
+
+    /// @notice repay loan
+    /// @param _daiAmount amount of loaned DAI to repay
+    function repay(uint256 _daiAmount) external {
+        if (_daiAmount > loans[msg.sender]) revert InvalidAmount();
+
+        unchecked {
+            loans[msg.sender] -= _daiAmount;
+        }
+
+        token.transferFrom(msg.sender, address(this), _daiAmount);
+
+        emit Repay(msg.sender, _daiAmount);
+    }
+
+    // ============== Restricted Functions ==============
+
+    /// @notice liquidate all collateral and loan if balance decreases a threshold
+    /// @dev can only be called by owner
+    /// @param borrower address of the borrower
+    function liquidate(address borrower) external {
+        if (msg.sender != owner) revert Unauthorised();
+
+        uint256 depositDaiAmount = applyExchangeRate(deposits[borrower]);
+
+        if (depositDaiAmount >= loans[borrower]) revert Safe();
+
+        delete deposits[borrower];
+        delete loans[borrower];
+        emit Liquidate(borrower);
+    }
+}
---
+++ src/test/CollateralizedVault.t.sol
@@ -0,0 +1,554 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.12;
+
+import {CollateralizedVault} from "../CollateralizedVault.sol";
+import {Dai} from "../Dai.sol";
+import {MockPriceFeed} from "../MockPriceFeed.sol";
+import {DSTestPlus} from "./utils/DSTestPlus.sol";
+
+contract TestCollateralizedVault is DSTestPlus {
+
+    address public constant vaultOwner = address(0xabcd);
+    address public constant mockUser = address(0x1234);
+    uint256 public constant INITIAL_VAULT_DAI_BALANCE = 1000000;
+    uint256 public constant INITIAL_SETUP_BALANCE = 100 ether;
+    int256 public constant INITIAL_EXCHANGE_RATE = 3000;
+
+    uint256 public constant CHAIN_ID = 4;
+    Dai public dai;
+    MockPriceFeed public priceFeed;
+    CollateralizedVault public vault;
+
+    function setUp() public {
+        dai = new Dai(CHAIN_ID);
+        priceFeed = new MockPriceFeed(INITIAL_EXCHANGE_RATE, 8);
+
+        vm.prank(vaultOwner);
+        vault = new CollateralizedVault(address(dai), address(priceFeed));
+    }
+
+    function _setUpBalances() private {
+        vm.deal(address(this), INITIAL_SETUP_BALANCE);
+        dai.mint(address(vault), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testDeposit() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(address(this).balance, INITIAL_SETUP_BALANCE - 10 ether);
+    }
+
+    function testDepositMultipleDeposits() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+        vault.deposit{value: 20 ether}();
+
+        assertEq(vault.deposits(address(this)), 30 ether);
+        assertEq(address(this).balance, INITIAL_SETUP_BALANCE - 30 ether);
+    }
+
+    function testDepositMultipleAddress() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        vm.deal(mockUser, 20 ether);
+        vm.prank(mockUser);
+        vault.deposit{value: 20 ether}();
+
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(vault.deposits(mockUser), 20 ether);
+        assertEq(address(this).balance, INITIAL_SETUP_BALANCE - 10 ether);
+        assertEq(mockUser.balance, 0 ether);
+    }
+
+    function testDepositZero() public {
+        _setUpBalances();
+
+        vault.deposit();
+
+        assertEq(vault.deposits(address(this)), 0);
+        assertEq(address(this).balance, INITIAL_SETUP_BALANCE);
+    }
+
+    function testDepositFuzz(uint256 amount) public {
+        vm.deal(address(this), amount);
+        vault.deposit{value: amount}();
+
+        assertEq(vault.deposits(address(this)), amount);
+        assertEq(address(this).balance, 0);
+    }
+
+    function testDepositMultipleFuzz(uint256 amount) public {
+        vm.assume(amount < type(uint256).max >> 1);
+        vm.deal(address(this), 2 * amount);
+
+        vault.deposit{value: amount}();
+        vault.deposit{value: amount}();
+
+        assertEq(vault.deposits(address(this)), amount * 2);
+    }
+
+    function testApplyExchangeRate() public {
+        uint256 amount = vault.applyExchangeRate(1 ether);
+        assertEq(amount, uint256(INITIAL_EXCHANGE_RATE * 1 ether) / 1e18);
+    }
+
+    function testApplyExchangeRateFuzz(uint256 amount) public {
+        vm.assume(amount < type(uint256).max / uint256(INITIAL_EXCHANGE_RATE));
+
+        uint256 expected = uint256(INITIAL_EXCHANGE_RATE) * amount;
+        uint256 actual = vault.applyExchangeRate(amount);
+
+        assertEq(actual, expected / 1e18);
+    }
+
+    function testBorrow() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 10 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    function testBorrowPartial() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 partialLoanAmount = (10 * uint256(INITIAL_EXCHANGE_RATE)) >> 1;
+        vault.borrow(partialLoanAmount);
+
+        assertEq(vault.loans(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - partialLoanAmount);
+    }
+
+    function testBorrowMultiple() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 totalLoanAmount = 10 * uint256(INITIAL_EXCHANGE_RATE);
+        uint256 partialLoanAmount = totalLoanAmount >> 1;
+
+        vault.borrow(partialLoanAmount);
+        vault.borrow(partialLoanAmount);
+
+        assertEq(vault.loans(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - totalLoanAmount);
+    }
+
+    function testBorrowInsufficientCollateral() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 totalLoanAmount = 10 * uint256(INITIAL_EXCHANGE_RATE);
+        uint256 partialLoanAmount = totalLoanAmount >> 1;
+
+        vault.borrow(partialLoanAmount);
+
+        vm.expectRevert(abi.encodeWithSignature("InsufficientCollateral()"));
+        vault.borrow(partialLoanAmount + 1);
+
+        assertEq(vault.loans(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - partialLoanAmount);
+    }
+
+    function testBorrowAppreciateETH() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        // set new rate
+        priceFeed.modifyExchangeRate(3200);
+        (, int256 exchangeRate, , , ) = priceFeed.latestRoundData();
+
+        uint256 loanAmount = 10 * uint256(exchangeRate);
+
+        vault.borrow(loanAmount);
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    function testBorrowMultipleAppreciateETH() public {
+        _setUpBalances();
+        uint256 totalDepositETH = 10;
+        uint256 partialLoanAmountETH = 5;
+
+        vault.deposit{value: 10 ether}();
+
+
+        uint256 partialLoanAmount1 = partialLoanAmountETH * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(partialLoanAmount1);
+
+        // set new rate
+        priceFeed.modifyExchangeRate(3200);
+        (, int256 exchangeRate, , , ) = priceFeed.latestRoundData();
+
+        uint256 partialLoanAmount2 = (totalDepositETH * uint256(exchangeRate)) - (partialLoanAmountETH * uint256(INITIAL_EXCHANGE_RATE));
+
+        vault.borrow(partialLoanAmount2);
+
+        uint256 totalLoanAmount = partialLoanAmount1 + partialLoanAmount2;
+
+        assertEq(vault.loans(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - totalLoanAmount);
+    }
+
+    function testBorrowDepreciateETH() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        // set new rate
+        priceFeed.modifyExchangeRate(2800);
+        (, int256 exchangeRate, , , ) = priceFeed.latestRoundData();
+
+        uint256 loanAmount = 10 * uint256(exchangeRate);
+        vault.borrow(loanAmount);
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    function testBorrowMultipleDepreciateETH() public {
+        _setUpBalances();
+
+        uint256 totalDepositETH = 10;
+        uint256 partialLoanAmountETH = 5;
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 partialLoanAmount1 = partialLoanAmountETH * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(partialLoanAmount1);
+
+        // set new rate
+        priceFeed.modifyExchangeRate(2800);
+        (, int256 exchangeRate, , , ) = priceFeed.latestRoundData();
+
+        uint256 partialLoanAmount2 = (totalDepositETH * uint256(exchangeRate)) - (partialLoanAmountETH * uint256(INITIAL_EXCHANGE_RATE));
+        vault.borrow(partialLoanAmount2);
+
+        uint256 totalLoanAmount = partialLoanAmount1 + partialLoanAmount2;
+
+        assertEq(vault.loans(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - totalLoanAmount);
+    }
+
+    function testBorrowFuzz(uint256 depositAmount) public {
+        vm.assume(depositAmount < type(uint256).max / (1e18 * uint256(INITIAL_EXCHANGE_RATE)));
+        uint256 depositETH = depositAmount * 1e18;
+        uint256 daiMint = depositETH * uint256(INITIAL_EXCHANGE_RATE);
+
+        vm.deal(address(this), depositETH);
+        dai.mint(address(vault), daiMint);
+
+        vault.deposit{value: depositETH}();
+
+        uint256 loanAmount = depositAmount * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), daiMint - loanAmount);
+    }
+
+    function testBorrowPartialFuzz(uint256 depositAmount) public {
+        vm.assume(depositAmount < type(uint256).max / (1e18 * uint256(INITIAL_EXCHANGE_RATE)));
+        uint256 depositETH = depositAmount * 1e18;
+        uint256 daiMint = depositETH * uint256(INITIAL_EXCHANGE_RATE);
+
+        vm.deal(address(this), depositETH);
+        dai.mint(address(vault), daiMint);
+
+        vault.deposit{value: depositETH}();
+
+        uint256 partialLoanAmount = (depositAmount * uint256(INITIAL_EXCHANGE_RATE)) >> 1;
+        vault.borrow(partialLoanAmount);
+
+        assertEq(vault.loans(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), daiMint - partialLoanAmount);
+    }
+
+    function testBorrowMultipleFuzz(uint256 depositAmount) public {
+        vm.assume(depositAmount < type(uint256).max / (1e18 * uint256(INITIAL_EXCHANGE_RATE)));
+        uint256 depositETH = depositAmount * 1e18;
+        uint256 daiMint = depositETH * uint256(INITIAL_EXCHANGE_RATE);
+
+        vm.deal(address(this), depositETH);
+        dai.mint(address(vault), daiMint);
+
+        vault.deposit{value: depositETH}();
+
+        uint256 totalLoanAmount = depositAmount * uint256(INITIAL_EXCHANGE_RATE);
+        uint256 partialLoanAmount = totalLoanAmount >> 1;
+
+        vault.borrow(partialLoanAmount);
+        vault.borrow(partialLoanAmount);
+
+        assertEq(vault.loans(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(this)), totalLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), daiMint - totalLoanAmount);
+    }
+
+    function testBorrowInsufficientCollateralFuzz(uint256 depositAmount) public {
+        vm.assume(depositAmount < type(uint256).max / (1e18 * uint256(INITIAL_EXCHANGE_RATE)));
+        uint256 depositETH = depositAmount * 1e18;
+        uint256 daiMint = depositETH * uint256(INITIAL_EXCHANGE_RATE);
+
+
+        vm.deal(address(this), depositETH);
+        dai.mint(address(vault), daiMint);
+
+        vault.deposit{value: depositETH}();
+
+        uint256 totalLoanAmount = depositAmount * uint256(INITIAL_EXCHANGE_RATE);
+        uint256 partialLoanAmount = totalLoanAmount >> 1;
+
+        vault.borrow(partialLoanAmount);
+
+        vm.expectRevert(abi.encodeWithSignature("InsufficientCollateral()"));
+        vault.borrow(partialLoanAmount + 1);
+
+        assertEq(vault.loans(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(this)), partialLoanAmount);
+        assertEq(dai.balanceOf(address(vault)), daiMint - partialLoanAmount);
+    }
+
+    function testWithdraw() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        vault.withdraw(10 ether);
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 0 ether);
+        assertEq(dai.balanceOf(address(this)), 0);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testWithdrawPartial() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        vault.withdraw(5 ether);
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 5 ether);
+        assertEq(dai.balanceOf(address(this)), 0);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testWithdrawMultiple() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        vault.withdraw(5 ether);
+        vault.withdraw(5 ether);
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 0 ether);
+        assertEq(dai.balanceOf(address(this)), 0);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testWithdrawInsufficientBalance() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        vm.expectRevert(abi.encodeWithSignature("InsufficientBalance()"));
+        vault.withdraw(11 ether);
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(dai.balanceOf(address(this)), 0);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testWithdrawMultipleInsufficientBalance() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        vault.withdraw(5 ether);
+
+        vm.expectRevert(abi.encodeWithSignature("InsufficientBalance()"));
+        vault.withdraw(6 ether);
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 5 ether);
+        assertEq(dai.balanceOf(address(this)), 0);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testWithdrawWithLoan() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 5 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        vault.withdraw(5 ether);
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(vault.deposits(address(this)), 5 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    function testWithdrawWithMultipleLoans() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 2 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+        vault.borrow(loanAmount);
+
+        vault.withdraw(6 ether);
+
+        assertEq(vault.loans(address(this)), loanAmount * 2);
+        assertEq(vault.deposits(address(this)), 4 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount * 2);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount * 2);
+    }
+
+    function testWithdrawWithMultipleLoansAndInsufficientBalance() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 2 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+        vault.borrow(loanAmount);
+
+        vm.expectRevert(abi.encodeWithSignature("InsufficientBalance()"));
+        vault.withdraw(7 ether);
+
+        assertEq(vault.loans(address(this)), loanAmount * 2);
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount * 2);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount * 2);
+    }
+
+    function testRepay() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 5 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        dai.approve(address(vault), loanAmount);
+        vault.repay(loanAmount);
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(dai.balanceOf(address(this)), 0);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE);
+    }
+
+    function testRepayPartial() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 5 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        uint256 repayAmount = 3 * uint256(INITIAL_EXCHANGE_RATE);
+        dai.approve(address(vault), repayAmount);
+        vault.repay(repayAmount);
+
+        assertEq(vault.loans(address(this)), loanAmount - repayAmount);
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount - repayAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount + repayAmount);
+    }
+
+    function testRepayInvalidAmount() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 5 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        vm.expectRevert(abi.encodeWithSignature("InvalidAmount()"));
+        vault.repay(loanAmount + 1 ether);
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    function testLiquidate() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 5 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        priceFeed.modifyExchangeRate(100);
+        assertEq(vault.applyExchangeRate(vault.deposits(address(this))), 1000);
+
+        vm.prank(vaultOwner);
+        vault.liquidate(address(this));
+
+        assertEq(vault.loans(address(this)), 0);
+        assertEq(vault.deposits(address(this)), 0 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    function testLiquidateUnauthorised() public {
+        vm.expectRevert(abi.encodeWithSignature("Unauthorised()"));
+        vault.liquidate(address(this));
+    }
+
+    function testLiquidateSafe() public {
+        _setUpBalances();
+
+        vault.deposit{value: 10 ether}();
+
+        uint256 loanAmount = 5 * uint256(INITIAL_EXCHANGE_RATE);
+        vault.borrow(loanAmount);
+
+        vm.expectRevert(abi.encodeWithSignature("Safe()"));
+        vm.prank(vaultOwner);
+        vault.liquidate(address(this));
+
+        assertEq(vault.loans(address(this)), loanAmount);
+        assertEq(vault.deposits(address(this)), 10 ether);
+        assertEq(dai.balanceOf(address(this)), loanAmount);
+        assertEq(dai.balanceOf(address(vault)), INITIAL_VAULT_DAI_BALANCE - loanAmount);
+    }
+
+    receive() external payable {}
+}
