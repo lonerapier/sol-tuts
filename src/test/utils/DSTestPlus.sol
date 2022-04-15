0c6d7b6179ae1eb37e04499e355d8759b0e7eea3
Sambhav Dusad <sambhavdusad24@gmail.com>
2022-04-15 15:54:53 +0530

    👽️ update `DSTestPlus` due to upstream `forge-std` changes

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -6,10 +6,9 @@ import {XConsole} from "./Console.sol";
 import {DSTest} from "@ds/test.sol";
 import {ERC20} from "@solmate/tokens/ERC20.sol";
 
-import {stdCheats, stdError} from "@std/stdlib.sol";
 import {Vm} from "@std/Vm.sol";
 
-contract DSTestPlus is DSTest, stdCheats {
+contract DSTestPlus is DSTest {
     XConsole console = new XConsole();
 
     /// @dev Use forge-std Vm logic
